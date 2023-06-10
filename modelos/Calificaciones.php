97f97c1ad0ab915ed25e8acc090f350ba7bdcf4c
dannycornelio <licdannycornelio@gmail.com>
2023-06-10 17:56:37 -0600

    modificacion del archivo calificaciones

--- modelos/Calificaciones.php
+++ modelos/Calificaciones.php
@@ -1,7 +1,8 @@
+
 <?php
 require_once 'Conexion.php';
 
-class Detalle extends Conexion{
+class Calificacion extends Conexion{
     public $id_calificaciones;
     public $calif_alumno;
     public $calif_materia;
@@ -16,26 +17,45 @@ class Detalle extends Conexion{
         $this->calif_materia = $args['calif_materia'] ?? '';
         $this->calif_punteo = $args['calif_punteo'] ?? '';
         $this->calif_resultado = $args['calif_resultado'] ?? '';
-        $this->detalle_situacion = $args['detalle_situacion'] ?? '';
+        $this->detalle_situacion = $args['detalle_situacion'] ?? '1';
     }
 
     public function guardar(){
-        $sql = "INSERT INTO calificaciones(calif_alumno, calif_materia, calif_punteo, calif_resultado, detalle_situacion) 
-                VALUES ('$this->calif_alumno', '$this->calif_materia', '$this->calif_punteo', '$this->calif_resultado', '$this->detalle_situacion')";
+        $sql = "INSERT INTO calificaciones(calif_alumno, calif_materia, calif_punteo, calif_resultado) VALUES ($this->calif_alumno, $this->calif_materia, $this->calif_punteo, '$this->calif_resultado')";
         $resultado = self::ejecutar($sql);
         return $resultado;
     }
 
     public function buscar(){
-        $sql = "SELECT alumnos.alu_nombre || ' ' || alumnos.alu_apellido AS Nombre, materias.ma_nombre, AVG(calificaciones.calif_punteo) AS Promedio
-                FROM calificaciones
-                JOIN alumnos ON calificaciones.calif_alumno = alumnos.id_alumnos
-                JOIN materias ON calificaciones.calif_materia = materias.id_materias
-                WHERE calificaciones.detalle_situacion = '1'
-                GROUP BY alumnos.alu_nombre, alumnos.alu_apellido, materias.ma_nombre";
+        $sql = "SELECT * FROM calificaciones WHERE detalle_situacion = '1'";
+
+        if($this->id_calificaciones != null){
+            $sql .= " AND id_calificaciones = $this->id_calificaciones";
+        }
+
+        if($this->calif_alumno != ''){
+            $sql .= " AND calif_alumno = $this->calif_alumno";
+        }
+
+        if($this->calif_materia != ''){
+            $sql .= " AND calif_materia = $this->calif_materia";
+        }
 
         $resultado = self::servir($sql);
         return $resultado;
     }
+
+    public function modificar(){
+        $sql = "UPDATE calificaciones SET calif_alumno = $this->calif_alumno, calif_materia = $this->calif_materia, calif_punteo = $this->calif_punteo, calif_resultado = '$this->calif_resultado' WHERE id_calificaciones = $this->id_calificaciones";
+        
+        $resultado = self::ejecutar($sql);
+        return $resultado;
+    }
+
+    public function eliminar(){
+        $sql = "UPDATE calificaciones SET detalle_situacion = '0' WHERE id_calificaciones = $this->id_calificaciones";
+        
+        $resultado = self::ejecutar($sql);
+        return $resultado;
+    }
 }
-?>
