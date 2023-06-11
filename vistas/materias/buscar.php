1a77c782154d3cdc6a4b2d625c5818b71567ceb1
dannycornelio <licdannycornelio@gmail.com>
2023-06-10 22:37:47 -0600

    buscar de vista materias

---
+++ vistas/materias/buscar.php
@@ -0,0 +1,20 @@
+<?php include_once '../../includes/header.php'?>
+<?php include_once '../../includes/navbar.php'?>
+    <div class="container">
+        <h1 class="text-center">Buscar productos</h1>
+        <div class="row justify-content-center">
+            <form action="/final_cornelio/controladores/materias/buscar.php" method="GET" class="col-lg-8 border bg-light p-3">
+                <div class="row mb-3">
+                    <div class="col">
+                        <label for="ma_nombre">Nombre de la Materia</label>
+                        <input type="text" name="ma_nombre" id="ma_nombre" class="form-control">
+                    </div>
+                </div>
+                    <div class="col">
+                        <button type="submit" class="btn btn-info w-100">Buscar</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    <?php include_once '../../includes/footer.php'?>
