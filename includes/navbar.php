7e0c3b4fc56dcf0af8a9940e775675af81013e8c
dannycornelio <licdannycornelio@gmail.com>
2023-06-10 18:15:01 -0600

    creacion del navba

---
+++ includes/navbar.php
@@ -0,0 +1,57 @@
+<nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="#">Escuela de Informatica</a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        calificaciones
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/practica_9/vistas/ventas/index.php">Ingresar calificaciones</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/practica_9/vistas/ventas/buscar.php">Buscar calificaciones</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/practica_9/controladores/ventas/detalle_ventas.php">Detalle de calificaciones</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Materias
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/practica_9/vistas/productos/index.php">Crear Materia</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/practica_9/vistas/productos/buscar.php">Buscar Materia</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Alumnos
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/practica_9/vistas/clientes/index.php">Crear Alumnos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/practica_9/vistas/clientes/buscar.php">Buscar Alumnos</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
